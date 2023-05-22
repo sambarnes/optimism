8202b3d6a310e8ac87e9469ae86e4ec0861e4416
Andreas Bigger <abigger87@gmail.com>
2023-05-22 12:00:37 -0700

    Spawn catchup routine when a block is missed.

--- op-challenger/challenger/traversal.go
+++ op-challenger/challenger/traversal.go
@@ -2,6 +2,7 @@ package challenger
 
 import (
 	"context"
+	"math/big"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
@@ -23,10 +24,11 @@ type LogTraversal interface {
 
 // logTraversal implements LogTraversal.
 type logTraversal struct {
-	log    log.Logger
-	client *ethclient.Client
-	query  ethereum.FilterQuery
-	quit   chan struct{}
+	log             log.Logger
+	client          *ethclient.Client
+	query           ethereum.FilterQuery
+	quit            chan struct{}
+	lastBlockNumber big.Int
 }
 
 // NewLogTraversal creates a new log traversal.
@@ -59,7 +61,7 @@ func (l *logTraversal) buildBackoffStrategy() backoff.Strategy {
 	}
 }
 
-// fetchBlock gracfetches a block with a backoff.
+// fetchBlock gracefully fetches a block with a backoff.
 func (l *logTraversal) fetchBlock(ctx context.Context, hash common.Hash) (*types.Block, error) {
 	backoffStrategy := l.buildBackoffStrategy()
 	var block *types.Block
@@ -114,41 +116,101 @@ func (l *logTraversal) Start(ctx context.Context, done chan struct{}, handleLog 
 		l.log.Error("failed to subscribe to new heads", "err", err)
 		return err
 	}
-	go func() {
-		for {
-			select {
-			case <-l.quit:
-				l.log.Info("stopping log traversal: received quit signal")
+	go l.onNewHead(ctx, headers, handleLog)
+	go l.unsubscribeOnDone(done, sub)
+	return nil
+}
+
+// onNewHead handles a new [types.Header].
+func (l *logTraversal) onNewHead(ctx context.Context, headers chan *types.Header, handleLog func(*types.Log) error) {
+	for {
+		select {
+		case <-l.quit:
+			l.log.Info("stopping log traversal: received quit signal")
+			return
+		case header := <-headers:
+			l.log.Info("received new head", "number", header.Number)
+			l.dispatchNewHead(ctx, header, handleLog, true)
+		}
+	}
+}
+
+// fetchHeaderByNumber gracefully fetches a block header by number with a backoff.
+func (l *logTraversal) fetchHeaderByNumber(ctx context.Context, num *big.Int) (*types.Header, error) {
+	backoffStrategy := l.buildBackoffStrategy()
+	var header *types.Header
+	err := backoff.DoCtx(ctx, 5, backoffStrategy, func() error {
+		ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+		defer cancel()
+		var err error
+		header, err = l.client.HeaderByNumber(ctx, num)
+		return err
+	})
+	return header, err
+}
+
+// spawnCatchup spawns a new goroutine to "catchup" for missed/skipped blocks.
+func (l *logTraversal) spawnCatchup(ctx context.Context, start *big.Int, end *big.Int, handleLog func(*types.Log) error) {
+	for {
+		// Break if we've caught up (start > end).
+		if start.Cmp(end) == 1 {
+			l.log.Info("caught up")
+			return
+		}
+
+		// Fetch the header with a backoff
+		header, err := l.fetchHeaderByNumber(ctx, start)
+		if err != nil {
+			l.log.Error("failed to fetch header", "err", err)
+			return
+		}
+		l.dispatchNewHead(ctx, header, handleLog, false)
+
+		// Increment to the next block to catch up to
+		start = start.Add(start, big.NewInt(1))
+	}
+}
+
+// dispatchNewHead dispatches a new head.
+func (l *logTraversal) dispatchNewHead(ctx context.Context, header *types.Header, handleLog func(*types.Log) error, allowCatchup bool) {
+	block, err := l.fetchBlock(ctx, header.Hash())
+	if err != nil {
+		l.log.Error("failed to fetch block", "err", err)
+		return
+	}
+	expectedBlockNumber := l.lastBlockNumber.Add(&l.lastBlockNumber, big.NewInt(1))
+	currentBlockNumber := block.Number()
+	if block.Number().Cmp(expectedBlockNumber) == 1 {
+		l.log.Warn("detected skipped block", "expectedBlockNumber", expectedBlockNumber, "currentBlockNumber", currentBlockNumber)
+		if allowCatchup {
+			endBlockNum := currentBlockNumber.Sub(currentBlockNumber, big.NewInt(1))
+			l.log.Warn("spawning catchup thread in range [%d, %d]", expectedBlockNumber, endBlockNum)
+			go l.spawnCatchup(ctx, expectedBlockNumber, endBlockNum, handleLog)
+		} else {
+			l.log.Warn("missed block detected, but catchup disabled")
+		}
+	}
+	receipts, err := l.fetchTransactionReceipts(ctx, block.Transactions())
+	if err != nil {
+		l.log.Error("failed to fetch receipts", "err", err)
+		return
+	}
+	for _, receipt := range receipts {
+		for _, log := range receipt.Logs {
+			err := handleLog(log)
+			if err != nil {
+				l.log.Error("failed to handle log", "err", err)
 				return
-			case header := <-headers:
-				block, err := l.fetchBlock(ctx, header.Hash())
-				if err != nil {
-					l.log.Error("failed to fetch block", "err", err)
-					return
-				}
-				receipts, err := l.fetchTransactionReceipts(ctx, block.Transactions())
-				if err != nil {
-					l.log.Error("failed to fetch receipts", "err", err)
-					return
-				}
-				for _, receipt := range receipts {
-					for _, log := range receipt.Logs {
-						err := handleLog(log)
-						if err != nil {
-							l.log.Error("failed to handle log", "err", err)
-							return
-						}
-					}
-				}
 			}
 		}
-	}()
-	go func() {
-		<-done
-		l.log.Info("stopping log traversal: received done signal")
-		sub.Unsubscribe()
-	}()
-	return nil
+	}
+}
+
+// unsubscribeOnDone unsubscribes from new heads when done.
+func (l *logTraversal) unsubscribeOnDone(done chan struct{}, sub ethereum.Subscription) {
+	<-done
+	l.log.Info("stopping log traversal: received done signal")
+	sub.Unsubscribe()
 }
 
 // Quit quits the log traversal.
